60eea446099266c2db70d3ae8401463ca2bbf5fd
Pontus Erlingsson <erpo16@nti.umea>
2018-12-05 12:41:09 +0100

    basic program klart

--- chiffer.py
+++ chiffer.py
@@ -5,22 +5,26 @@ crypt = []
 meny = 0
 
 while meny !=3:
-    meny = int(input("1 för att krypter \n 2 för att dekryptera"))
+    meny = int(input(" 1 för att krypter:  \n 2 för att dekryptera:  \n 3 för att avsluta: "))
     if meny == 1:
-        key = int(input("skriv in en siffra för att kryptera"))
-        word = input("skriv in ett ord du vill kryptera")
+        key = int(input("skriv in en siffra för att kryptera "))
+        word = input("skriv in ett ord du vill kryptera ")
         for letter in word:
             letters.append(ord(letter) + key)
 
         print(letters)
+        
     elif meny == 2:
         key = int(input("skriv in en siffra för att dekryptera"))
-        word = input("skriv in ett ord du vill dekryptera")
-    
-    for l in letters:
-        crypt.append(chr(l -key))
+        word = input("skriv in siffrorna för dom krypterade bokstävera ")
+        for l in letters:
+            crypt.append(chr(l -key))
+        print(crypt)
+    elif meny ==3:
+        print("nu avslutas programmet")
 
-    print(crypt)
+
+        
 
 
 
